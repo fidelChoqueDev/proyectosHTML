488ef5e338fa51c0a2eeb4bc8b41811c218fdb6d
Fidel Choque <82333258+fidelChoqueDev@users.noreply.github.com>
2026-07-12 21:15:45 +0200

    add style and command javascrip

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-const contenedor = document.getElementById('contenedor');
+/* Selecciona un elemento por su id*/
+const titulo = document.getElementById('titulo');
+console.log (titulo.innerText);
+
+const animes = document.getElementsByClassName('animes');
+console.log(animes[2].id);
+
+const misAnimes = document.getElementsByTagName('li')
+console.log(misAnimes)
