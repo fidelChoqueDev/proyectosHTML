17d162fe71f243efa207ee4ae28b6847350dbc1e
Fidel Choque <82333258+fidelChoqueDev@users.noreply.github.com>
2026-07-26 19:39:48 +0200

    add db.js and imagen new the project

---
+++ db.js
@@ -0,0 +1,51 @@
+const dbAnime = {
+    items : [
+        {
+					id: 0,
+          title : "Super Campeones",
+    			description : "(conocido originalmente en Japón como Captain Tsubasa y en España como Oliver y Benji) es un legendario anime deportivo creado por Yōichi Takahashi. La historia sigue a Oliver Atom (Tsubasa Ozora), un prodigioso niño obsesionado con el fútbol cuyo sueño es convertirse en el mejor jugador del planeta y ganar la Copa del Mundo con la selección de Japón. A lo largo de la serie, Oliver pasa de disputar apasionantes torneos escolares a competir a nivel profesional e internacional, enfrentándose a rivales icónicos como Steve Hyuga o el portero Benji Price. Es famoso por sus jugadas espectaculares que desafían la física (como el tiro del tigre o el tiro con efecto) y campos de juego aparentemente infinitos.",
+					imagen: "C:\Users\HP PRODESK 400 G2\Documents\Proyectos\proyectosHTML\imagenes\captain-tsubasa"
+        },
+				{
+					id: 1,
+          title : "One Piece",
+    			description : "descripcion"
+        },
+				{
+					id: 2,
+          title : "Naruto",
+    			description : "descripcion"
+        },
+				{
+					id: 3,
+          title : "Jujutsu Kaisen",
+    			description : "descripcion"
+        },
+				{
+					id: 4,
+          title : "My Hero Academy",
+    			description : "descripcion"
+        },
+				{
+					id: 5,
+          title : "Haikyuu",
+    			description : "descripcion"
+        },
+				{
+					id: 6,
+          title : "Demon Slayer",
+    			description : "descripcion"
+        },
+				{
+					id: 7,
+          title : "Dragon BAll",
+    			description : "descripcion"
+        },
+				{
+					id: 8,
+          title : "Caballeros del Zodiaco",
+    			description : "descripcion"
+        },
+    ]
+    
+}
